f4c57d3b576463539cfafe91a6d07bed6f02e336
M O Faruque Sarker <f.saker@ucl.ac.uk>
2014-09-09 13:54:36 +0100

    updated check slave start/stop

--- bin/check_slave_start.sh
+++ bin/check_slave_start.sh
@@ -14,13 +14,13 @@ source ${SCRIPT_CONFIG_PATH}/backup_tasks.conf
 source ${SCRIPT_BIN_PATH}/backup_tasks_lib.sh
 
 # check MySQL slave
-slave_status=$(mysql -u $MYSQL_USER -p$MYSQL_PASS -e "SHOW GLOBAL STATUS like 'slave_running' " 2>&1 | grep Slave_running | awk -F" " {'print $2 '})
+slave_status=$(mysql -h `hostname -f` -u $MYSQL_USER -p$MYSQL_PASS -e "SHOW GLOBAL STATUS like 'slave_running' " 2>&1 | grep Slave_running | awk -F" " {'print $2 '})
 handle_event "INFO"  "$TIMESTAMP: INFO MySQL Slave status: $slave_status" 
-if [ "$slave_status"  == OFF ]; then 
+if [[ -n  "$slave_status" && "$slave_status"  == 'OFF' ]]; then 
     handle_event "INFO"  "$TIMESTAMP: INFO Slave is found stopped. Starting now..." 
-        start_slave=$(mysql -u $MYSQL_USER -p$MYSQL_PASS -e "START SLAVE" 2>&1)
-    slave_status=$(mysql -u $MYSQL_USER -p$MYSQL_PASS -e "SHOW GLOBAL STATUS like 'slave_running' " 2>&1 | grep Slave_running | awk -F" " {'print $2 '})
-    if [ $slave_status  == 'ON' ]; then 
+        start_slave=$(mysql -h `hostname -f` -u $MYSQL_USER -p$MYSQL_PASS -e "START SLAVE SQL_THREAD" 2>&1)
+    slave_status=$(mysql -h `hostname -f` -u $MYSQL_USER -p$MYSQL_PASS -e "SHOW GLOBAL STATUS like 'slave_running' " 2>&1 | grep Slave_running | awk -F" " {'print $2 '})
+    if [[ -n  "$slave_status" && "$slave_status"   == 'ON' ]]; then 
 	    handle_event "INFO"  "$TIMESTAMP: INFO Slave has been started." 
         else {
         	handle_event "ERROR"  "$TIMESTAMP: ERROR Slave is not started. status is $slave_status." 
--- bin/check_slave_stop.sh
+++ bin/check_slave_stop.sh
@@ -16,29 +16,29 @@ source ${SCRIPT_BIN_PATH}/backup_tasks_lib.sh
 ## Check and stop MySQL slave
 # Check if the replication link is broken
 handle_event "INFO" "$TIMESTAMP: INFO Checking MySQL Master-Slave link."
-seconds_behind_master=$(mysql -u $MYSQL_USER -p$MYSQL_PASS -e "SHOW SLAVE STATUS\G" 2>&1 | grep "Seconds_Behind_Master" | awk -F": " {' print $2 '})
-if [ "$seconds_behind_master" == "NULL" ]; then {
+seconds_behind_master=$(mysql -h `hostname -f` -u $MYSQL_USER -p$MYSQL_PASS -e "SHOW SLAVE STATUS\G" 2>&1 | grep "Seconds_Behind_Master" | awk -F": " {' print $2 '})
+handle_event "INFO" "$TIMESTAMP: INFO Slave replication is $seconds_behind_master seconds behind." 
+
+if [ "$seconds_behind_master" = "NULL" ]; then {
     handle_event "ERROR" "$TIMESTAMP: ERROR Slave replication broken." 
     if ! $IGNORE_NON_FATAL; then
     	exit 1 # 
     fi
     }
 else
-    if [ "$seconds_behind_master" -gt "$SLAVE_ACCEPTABLE_LAG" ]; then
+    if [[ -n "$seconds_behind_master" && "$seconds_behind_master" -gt "$SLAVE_ACCEPTABLE_LAG" ]]; then
 	    handle_event "INFO" "$TIMESTAMP: INFO Slave replication is $seconds_behind_master seconds behind." 
     else
 	    handle_event "INFO" "$TIMESTAMP: INFO Slave replication is up-to-date." 
-	    mysql -u $MYSQL_USER -p$MYSQL_PASS -e "STOP SLAVE" 2>&1 || {
-		handle_event "ERROR" "$TIMESTAMP: ERROR Slave stop failed" 
-		exit 1
-	    }
-	    slave_status=$(mysql -u $MYSQL_USER -p$MYSQL_PASS -e "SHOW GLOBAL STATUS like 'slave_running' " 2>&1 | grep Slave_running | awk -F" " {'print $2 '})
-	    if [ $slave_status  == 'OFF' ]; then 
+	    mysql -h `hostname -f` -u $MYSQL_USER -p$MYSQL_PASS -e "STOP SLAVE SQL_THREAD" 2>&1 # Warning messages causes the scrip to abort
+	    # Error hadling is skipped as the following commands checks the success/failure of this command
+	    slave_status=$(mysql -h `hostname -f` -u $MYSQL_USER -p$MYSQL_PASS -e "SHOW GLOBAL STATUS like 'slave_running' " 2>/dev/null | grep Slave_running | awk -F" " {'print $2 '})
+	    if [[ -n "$slave_status" && "$slave_status" == 'OFF' ]]; then 
 		handle_event "INFO" "$TIMESTAMP: INFO Slave has stopped."
             else { 
-            	handle_event "ERROR" "$TIMESTAMP: ERROR Slave failed to stop." 
-	    	exit 1
+            	handle_event "INFO" "$TIMESTAMP:  Slave status: $slave_status -- last check" 
 	    }
 	    fi	
     fi # end seconds_behind_master check
+	
 fi # end of NULL value check
